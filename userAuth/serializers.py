0ba5a3f63057d5e535a85947e0e6ad6a26c02084
aernest njuki <74731728+Aernestnjuki@users.noreply.github.com>
2024-11-08 19:11:15 +0300

    created a serializer and a class based view to get the tokens when the user logs in

--- userAuth/serializers.py
+++ userAuth/serializers.py
@@ -1,7 +1,20 @@
 from .models import User, Profile
 from rest_framework.serializers import ModelSerializer
-
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+# returns an access_token and a refresh_token
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # add user information in the token
+        token['full_name'] = user.full_name
+        token['username'] = user.username
+        token['email'] = user.email
+
+        return token
 
 
 class UserSerializer(ModelSerializer):
--- userAuth/views.py
+++ userAuth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from userAuth import serializers as api_serializer
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = api_serializer.MyTokenObtainPairSerializer
+    permission_classes = [AllowAny]
